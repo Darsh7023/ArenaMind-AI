cdf4932b9ba4f3e938e48fc2165d4a6dd6c69587
Jagdish Kumar <jagdishsongar5315@gmail.com>
2026-07-15 12:24:49 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
+import Chat from "./pages/Chat/Chat";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Navigation from "./pages/Navigation/Navigation";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/navigation" element={<Navigation />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
