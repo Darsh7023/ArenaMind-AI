53abe4c3c81780479d0cb85345d91d9bf803fb32
Jagdish Kumar <jagdishsongar5315@gmail.com>
2026-07-16 15:48:47 +0530

    Updated Chat and ChatBot

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-function Dashboard() {
+function Chat() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-4xl font-bold">Dashboard Page</h1>
+      <h1 className="text-4xl font-bold">Chat Page</h1>
     </div>
   );
 }
 
-export default Dashboard;
+export default Chat;
