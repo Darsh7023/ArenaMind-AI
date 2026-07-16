b9c14977e313009deea01d625c64031c39479986
Jagdish Kumar <jagdishsongar5315@gmail.com>
2026-07-16 19:08:07 +0530

    Updated dashboard file

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
           📊 Live Stadium Dashboard
         </h1>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
 
           <div className="bg-slate-800 p-6 rounded-xl">
             <h2 className="text-xl font-semibold">👥 Crowd Level</h2>
@@ -31,6 +31,16 @@ function Dashboard() {
             <h2 className="text-xl font-semibold">🚨 Emergency Alerts</h2>
             <p className="text-3xl mt-3 text-cyan-400">1</p>
           </div>
+
+          <div className="bg-slate-800 p-6 rounded-xl">
+  <h2 className="text-xl font-semibold">🛡️ AI Security Status</h2>
+  <p className="text-3xl mt-3 text-green-400">Secure</p>
+</div>
+
+<div className="bg-slate-800 p-6 rounded-xl">
+  <h2 className="text-xl font-semibold">🌱 Energy Saving</h2>
+  <p className="text-3xl mt-3 text-green-400">75%</p>
+</div>
 
         </div>
       </div>
